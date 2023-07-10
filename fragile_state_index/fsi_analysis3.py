7c6c060a1fa29f74f5bcf39167ac0b90ee7b4a0b
Manuchehr Aminian <manuchehr.aminian@gmail.com>
2023-07-10 09:05:33 -0700

    Built predictive data set

--- fragile_state_index/fsi_analysis3.py
+++ fragile_state_index/fsi_analysis3.py
@@ -32,6 +32,11 @@ load_fsi.shorten_indicators(df_fsi)
 
 df_fsi = load_fsi.to_long(df_fsi)
 
+# convert to "multi-index" pivot table; a 3D object (Country, Year, Indicator);
+# accessed first by Year; then, rows are the data for that year.
+df_fsi_pivot = df_fsi.pivot_table(values='value', index='Country', columns=['Year','Indicator'])
+
+
 '''
 RESEARCH QUESTION:
     
@@ -59,8 +64,11 @@ L = 1
 df_p = df_tmk2.pivot_table(values='tmk', index='country', columns='year')
 df_p.fillna(0., inplace=True)
 
+tmk_minyear = df_p.columns.min()
+tmk_maxyear = 2023 # hard code...
+
 # pad with non-TMK years where needed
-for i in range(2006,2023 + 1):
+for i in range(tmk_minyear,tmk_maxyear + 1):
     if i not in df_p.columns:
         df_p[i] = np.zeros(df_p.shape[0])
 
@@ -74,15 +82,41 @@ _df_null = pandas.DataFrame(data = np.zeros( (len(non_tmk_countries), df_p.shape
 
 #
 df_p = df_p.append(_df_null)
+df_p.sort_index(inplace=True)
 
-X = []
-y = []
+# TODO: Code assumes row ordering is consistent.
+# CANNOT PROCEED WITH CODE AS-IS IF THIS IS NOT TRUE.
+assert all(df_p.index == df_fsi_pivot.index)
+
+#
+
+Xstack = []
+ystack = []
 # for each year i after 2006+k+L-1 (inclusive),
+for i in range(tmk_minyear+k+L-1, tmk_maxyear+1):
     # y_i : year i from TMK dataset
+    y_i = df_p[i].values
+    
     # X_i : year i-L-k+1 to year i-L inclusive from the FSI dataset.
+    # array dimensions go (year, country, indicator)
+    X_i = np.array([ df_fsi_pivot[i-L-k+1+j].values for j in range(k)])
+    # glue together data for a single country
+    X_i = np.concatenate(X_i, axis=1)
+    
+    # Filter out any missing data.
+    to_keep = ~np.any(np.isnan(X_i), axis=1)
+    
+    Xstack.append( X_i[to_keep] )
+    ystack.append( y_i[to_keep] )
+    
     # e.g. if k=2, L=1, then start at 2008 (prediction year); 
     # years 2008-1-2+1 (=2006) to 2008-1 (=2007) (TMI data years).
+#
 
+X = np.concatenate(Xstack, axis=0)
+y = np.concatenate(ystack)
 
+# must have no NaN at this point!
+assert( np.all(~np.isnan(X)) and np.all(~np.isnan(y)) )
 
-
+# final labeled data set (!!)
